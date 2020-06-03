3f4f4035208efaa129d1788ed37cb48fd3642026
Alex Tan <AlexTAN331@outlook.com>
2020-06-03 16:03:57 -0400

    in progress

--- src/FormEdit.js
+++ src/FormEdit.js
@@ -4,32 +4,58 @@ class FormEdit extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isEdit: true,
+      isEdit: false,
       isSave: false,
       isCancle: false,
       firstName: "",
       lastName: "",
     };
     this.handleOnClickEdit = this.handleOnClickEdit.bind(this);
+    this.handleOnClickEdit = this.handleOnClickSave.bind(this);
+    this.handleOnClickEdit = this.handleFirstName.bind(this);
+    this.handleOnClickEdit = this.handleLastName.bind(this);
   }
 
   handleOnClickEdit() {
-    if (this.state.isEdit == true) this.setState({ isEdit: false });
-    else this.setState({ isEdit: true });
+    this.setState({ isEdit: true });
   }
 
+  handleOnClickSave() {
+    this.setState({ isEdit: false });
+    if (this.state.isSave === true) this.setState({ isEdit: true });
+  }
+
+  handleFirstName(event) {
+    this.setState({ isEdit: false });
+    this.setState({ firstName: event.target.value });
+  }
+
+  handleLastName(event) {
+    this.setState({ isEdit: false });
+    this.setState({ firstName: event.target.value });
+  }
   render() {
     console.log("state: line22", this.state);
     return (
       <div>
         <div>First name:</div>
         <div>Last name:</div>
-        {this.state.isEdit ? (
+        {!this.state.isEdit ? (
           <button onClick={this.handleOnClickEdit}>Edit</button>
         ) : (
           <div>
-            <button onClick={this.handleOnClickEdit}>Save</button>
+            <button onClick={this.handleOnClickSave}>Save</button>
+            <input
+              type="text"
+              name="firstName"
+              onChange={this.handleOnClickEdit}
+            />
             <button onClick={this.handleOnClickEdit}>Cancel</button>{" "}
+            <input
+              type="text"
+              name={this.state.lastName}
+              onChange={this.handleLastName}
+            />
           </div>
         )}
       </div>
