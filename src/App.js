c575809b4d87c74b17df74234343acc121feedd9
Alex Tan <AlexTAN331@outlook.com>
2020-06-03 13:22:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import FormTest from "./FormTest";
+import SelectForm from "./SelectForm";
+import Decrement from "./Decrement";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <SelectForm />
+      </>
+    );
+  }
+}
+
+export default App;
